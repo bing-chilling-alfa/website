3f6dd7b380463d8450d2fd8363565d85fb5cbba3
ronniew <rwaebers@gmail.com>
2022-01-24 11:44:21 +0100

    fix routes

--- routes/web.php
+++ routes/web.php
@@ -13,37 +13,25 @@ use Illuminate\Support\Facades\Route;
 |
 */
 //
-//Route::view('/', 'pages.home')->name('home');
-//Route::view('/login', 'pages.login')->name('login');
+Route::view('/', 'pages.home')->name('home');
+Route::view('/login', 'pages.login')->name('login');
 //Route::view('/company', 'pages.company')->name('company');
 //Route::view('/college', 'pages.college')->name('college');
 //Route::view('/vacature', 'pages.vacature')->name('vacature');
-//Route::view('/contact', 'pages.contact')->name('contact');
+Route::view('/contact', 'pages.contact')->name('contact');
 
-Route::get('/', function() {
-    return view('pages.home');
+Route::get('/users/{id}/{name}', function ($id, $name) {
+    return $id . $name;
 });
 
-Route::get('/users/{id}/{name}', function($id, $name) {
-    return $id.$name;
-});
-
-Route::get('/college', function() {
+Route::get('/college', function () {
     return view('pages.college');
 });
 
-Route::get('/company', function() {
+Route::get('/company', function () {
     return view('pages.company');
 });
 
-Route::get('/vacature', function() {
+Route::get('/vacature', function () {
     return view('pages.vacature');
-});
-
-Route::get('/contact', function() {
-    return view('pages.contact');
-});
-
-Route::get('/login', function() {
-    return view('pages.login');
 });
