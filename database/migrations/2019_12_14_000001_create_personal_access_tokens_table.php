1d62aba61b6266e5212838de096a5af3f46a35b9
NickvanLoo28 <58033512+NickvanLoo@users.noreply.github.com>
2022-01-24 09:34:14 +0100

    Change maxlength

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -13,16 +13,17 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens',function (Blueprint $table){
+        Schema::create('personal_access_tokens', function (Blueprint $table){
             $table->id();
             $table->morphs('tokenable');
             $table->string('name');
-            $table->string('token',64)->unique();
+            $table->string('token') ->unique();
             $table->text('abilities')->nullable();
             $table->timestamp('last_used_at')->nullable();
             $table->timestamps();
-        });
-    }
+    });
+}
+
 
     /**
      * Reverse the migrations.
