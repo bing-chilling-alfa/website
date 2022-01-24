fc4b479ff6cfc17799cdb0ac252d5ae7075c2eb3
NickvanLoo28 <58033512+NickvanLoo@users.noreply.github.com>
2022-01-24 10:02:37 +0100

    Update header.blade.php

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,21 +1,13 @@
 <nav class="navbar navbar-expand-sm navbar-dark bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="javascript:void(0)">Logo</a>
+  <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#demo">
+  <i class="fas fa-bars"></i>
+  </button>
+    <a class="navbar-brand" href="/">Logo</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="mynavbar">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link" href="javascript:void(0)">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="javascript:void(0)">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="javascript:void(0)">Link</a>
-        </li>
-      </ul>
       <form class="d-flex">
         <input class="form-control me-2" type="text" placeholder="Zoeken naar stagebedrijf...">
         <button class="btn btn-primary" type="button">Zoeken</button>
@@ -23,23 +15,22 @@
     </div>
   </div>
 </nav> 
-<div class="offcanvas offcanvas-end" id="demo">
+<div class="offcanvas offcanvas-start" id="demo">
   <div class="offcanvas-header">
-    <h1 class="offcanvas-title">Heading</h1>
+    <h1 class="offcanvas-title">Menu</h1>
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas"></button>
   </div>
   <div class="offcanvas-body">
-    <p>Some text lorem ipsum.</p>
-    <p>Some text lorem ipsum.</p>
-    <p>Some text lorem ipsum.</p>
-    <button class="btn btn-secondary" type="button">A Button</button>
+  <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/login">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/contact">Contact</a>
+        </li>
+      </ul>
   </div>
-</div>
-
-<div class="container-fluid mt-3">
-  <h3>Right Offcanvas</h3>
-  <p>The .offcanvas-end class positions the offcanvas to the right of the page.</p>
-  <button class="btn btn-primary" type="button" data-bs-toggle="offcanvas" data-bs-target="#demo">
-    Toggle Right Offcanvas
-  </button>
 </div>
