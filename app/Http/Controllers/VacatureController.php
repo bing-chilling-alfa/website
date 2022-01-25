cd5ceeb023fa39a1c1fa567b5557efe718e9dba5
Fauve <fsetrodinomo@outlook.com>
2022-01-25 19:10:49 +0100

    recover read.me

--- app/Http/Controllers/VacatureController.php
+++ app/Http/Controllers/VacatureController.php
@@ -3,60 +3,77 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Vacature;
+use Illuminate\Routing\Controller;
 
 class VacatureController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function index()
     {
-        //
+        $vacatures = Vacature::all();
+        return view('$vacatures.index', compact('vacatures', 'vacatures'));
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function create()
     {
-        //
+        return view('vacatures.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function store(Request $request)
     {
-        //
+        $this->validate( $request,
+        [
+            'id' => 'required',
+            'business_id'=>'required|numeric',
+            'type-opleiding'=>'required',
+            'start-datum' => 'required',
+            'eind-datum' => 'required',
+            'timestamps' => 'required',
+        ]);
+        $input = $request->all();
+        Vacature::create($input);
+
+        return redirect()->route('vacatures.index');
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function show($id)
     {
-        //
+        $vacature = Vacature::findOrFail($id);
+        return view('vacatures.show', compact('vacature','vacature'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function edit($id)
     {
-        //
+        $vacature = Vacature::find($id);
+        return view('vacatures.edit',compact('vacature','vacature'));
     }
 
     /**
@@ -64,21 +81,39 @@ class VacatureController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function update(Request $request, $id)
     {
-        //
+        $vacature = Vacature::findOrFail($id);
+        $this->validate( $request,
+            [
+                'id' => 'required',
+                'business_id'=>'required|numeric',
+                'type-opleiding'=>'required',
+                'start-datum' => 'required',
+                'eind-datum' => 'required',
+                'timestamps' => 'required',
+            ]);
+        $input = $request->all();
+        $vacature->fill($input)->save();
+
+        return redirect()->route('vacatures.index');
+
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function destroy($id)
     {
-        //
+        $vacature = Vacature::findOrFail($id);
+
+        $vacature-> delete();
+        return redirect()->route('vacatures.index');
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,30 +20,34 @@ use Illuminate\Support\Facades\Route;
 //Route::view('/vacature', 'pages.vacature')->name('vacature');
 //Route::view('/contact', 'pages.contact')->name('contact');
 
-Route::get('/', function() {
-    return view('pages.home');
-});
 
-Route::get('/users/{id}/{name}', function($id, $name) {
-    return $id.$name;
-});
 
-Route::get('/college', function() {
-    return view('pages.college');
-});
 
-Route::get('/company', function() {
-    return view('pages.company');
-});
-
-Route::get('/vacature', function() {
-    return view('pages.vacature');
-});
-
-Route::get('/contact', function() {
-    return view('pages.contact');
-});
-
-Route::get('/login', function() {
-    return view('pages.login');
-});
+//
+//Route::get('/', function() {
+//    return view('pages.home');
+//});
+//
+//Route::get('/users/{id}/{name}', function($id, $name) {
+//    return $id.$name;
+//});
+//
+//Route::get('/college', function() {
+//    return view('pages.college');
+//});
+//
+//Route::get('/company', function() {
+//    return view('pages.company');
+//});
+//
+//Route::get('/vacature', function() {
+//    return view('pages.vacature');
+//});
+//
+//Route::get('/contact', function() {
+//    return view('pages.contact');
+//});
+//
+//Route::get('/login', function() {
+//    return view('pages.login');
+//});
