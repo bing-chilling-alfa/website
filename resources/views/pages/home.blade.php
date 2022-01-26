2a1d4cca0e55c6112d9370a3ef8a7fc650662ba4
xander <xander.perlee@gmail.com>
2022-01-26 09:55:15 +0100

    controller en scss files

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.default')
+
+@section('stylesheet')
+    {{--    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />--}}
+@endsection
+
+
+
+@section('content')
+    <div class="collapse navbar-collapse justify-content-center" id="mynavbar">
+        <form class="d-flex">
+            <input class="form-control me-2" type="text" placeholder="Zoeken naar stagebedrijf...">
+            <button class="btn btn-primary" type="button">Zoeken</button>
+        </form>
+    </div>
+    <img class="flex overflow-hidden w-100 h-50" src="https://wallpaperaccess.com/full/187161.jpg" alt="https://wallpaperaccess.com/full/187161.jpg">
+@endsection
