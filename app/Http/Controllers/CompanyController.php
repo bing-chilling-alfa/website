caa1fbc4dbe5d54aeda8a489a6e528759d805e4c
ronniew <rwaebers@gmail.com>
2022-01-27 11:12:16 +0100

    fix

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -15,15 +15,15 @@ class CompanyController extends Controller
     /**
      * Display a listing of the resource.
      *
-      * /@return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     * /@return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function index()
     {
         // $data = Company::all();
         // return view('companies', ['companies' => $data]);
 
-          $companies = Company::all();
-          return view('companies', compact('companies'));
+        $companies = Company::all();
+        return view('pages.companies', compact('companies'));
     }
 
     /**
@@ -44,17 +44,19 @@ class CompanyController extends Controller
      */
     public function store(Request $request)
     {
-        $this->validate( $request,
+        $this->validate(
+            $request,
             [
-                'id'=>'required',
-                'naam'=> 'required',
-                'telefoonnummer'=> 'required|numeric',
-                'email'=>'required',
-                'website'=>'required',
-                'postcode'=>'required',
-                'rememberToken'=>'required',
-                'timestamps' =>'required',
-            ]);
+                'id' => 'required',
+                'naam' => 'required',
+                'telefoonnummer' => 'required|numeric',
+                'email' => 'required',
+                'website' => 'required',
+                'postcode' => 'required',
+                'rememberToken' => 'required',
+                'timestamps' => 'required',
+            ]
+        );
         $input = $request->all();
         Company::create($input);
     }
@@ -68,7 +70,7 @@ class CompanyController extends Controller
     public function show($id)
     {
         $company = Company::findOrFail($id);
-        return view('companies.show', compact('company','company'));
+        return view('companies.show', compact('company', 'company'));
     }
 
     /**
@@ -80,8 +82,7 @@ class CompanyController extends Controller
     public function edit($id)
     {
         $company = Company::find($id);
-        return view('companies.edit', compact('company','company'));
-
+        return view('companies.edit', compact('company', 'company'));
     }
 
     /**
@@ -94,19 +95,21 @@ class CompanyController extends Controller
     public function update(Request $request, $id)
     {
         $companies = Company::findOrfail($id);
-        $this->validate( $request,
+        $this->validate(
+            $request,
             [
-                'id'=>'required',
-                'naam'=> 'required',
-                'telefoonnummer'=> 'required|numeric',
-                'email'=>'required',
-                'website'=>'required',
-                'postcode'=>'required',
-                'rememberToken'=>'required',
-                'timestamps' =>'required',
-            ]);
+                'id' => 'required',
+                'naam' => 'required',
+                'telefoonnummer' => 'required|numeric',
+                'email' => 'required',
+                'website' => 'required',
+                'postcode' => 'required',
+                'rememberToken' => 'required',
+                'timestamps' => 'required',
+            ]
+        );
         $input = $request->all();
-        $companies ->fill($input)->save();
+        $companies->fill($input)->save();
 
         return redirect()->route('company.index');
     }
@@ -120,7 +123,7 @@ class CompanyController extends Controller
     {
         $companies = Company::findOrFail($id);
 
-        $companies-> delete();
+        $companies->delete();
         return redirect()->route('companies.index');
     }
 }
