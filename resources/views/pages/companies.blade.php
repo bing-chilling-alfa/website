e9bb66a867b22661ebd703e8640943b25e28fe52
NickvanLoo28 <58033512+NickvanLoo@users.noreply.github.com>
2022-01-27 13:57:26 +0100

    update Companies.blade

--- resources/views/pages/companies.blade.php
+++ resources/views/pages/companies.blade.php
@@ -2,13 +2,13 @@
 
 @section('content')
 
-<div class="bedrijven">
+<div class="bedrijven align-items-sm-center">
 @foreach($companies as $company)
   <div class="row">
     <div class="card mb-3">
       <div class="row no-gutters">
         <div class="col-md-4">
-          <img src="https://s3-eu-west-1.amazonaws.com/tpd/logos/503ebec200006400051a4763/0x0.png" class="img-fluid rounded-start" alt="...">
+          <img src="https://s3-eu-west-1.amazonaws.com/tpd/logos/503ebec200006400051a4763/0x0.png" class="img-fluid rounded-start" alt="..." style="height:200px;">
         </div>
         <div class="col-md-8">
           <div class="card-body">
