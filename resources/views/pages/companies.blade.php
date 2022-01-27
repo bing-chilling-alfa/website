698a7525c1bbdae3439c1f509a412ae7d884f3ca
ronniew <rwaebers@gmail.com>
2022-01-27 10:32:25 +0100

    fix comapny

--- resources/views/pages/companies.blade.php
+++ resources/views/pages/companies.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.default')
 
 @section('content')
-@foreach($companies as $company)
+    @foreach ($companies as $company)
 
 
- {{$company['naam']}}
+        {{ $company['name'] }}
 
 
-@endforeach
+    @endforeach
 
 
-<div class="card w-75">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Button</a>
-  </div>
-</div>
+    <div class="card w-75">
+        <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Button</a>
+        </div>
+    </div>
 
 @endsection
