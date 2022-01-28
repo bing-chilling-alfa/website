dac76537c42135f5f46ed27b01c7371ee09437f0
Fauve <fsetrodinomo@outlook.com>
2022-01-28 11:15:49 +0100

    working on the photo slide for the company section at stageplek

--- resources/views/pages/Stageplek.blade.php
+++ resources/views/pages/Stageplek.blade.php
@@ -3,10 +3,23 @@
 @section('content')
 <div class="container">
 <div class="stageplek align-items-sm-center">
-    <div class="row">
-        <div class="col-sm-06" style="background-color:white" border-color:black;>
-            <p>testing</p>
+    <div id="stageCarrousel" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#dataCarrousel" data-slide-to="0" class="active"></li>
+            <li data-target="#dataCarrousel" data-slide-to="1" ></li>
+            <li data-target="#dataCarrousel" data-slide-to="2" ></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="item active">
+                <img src="la.jpg" alt="Los Angeles" style="width:100%;">
+            </div>
         </div>
+    </div>
+{{--    <div class="row">--}}
+{{--        <span class="label">Bingchilling</span>--}}
+{{--        <div class="col-sm-06" style="background-color:white" >--}}
+{{--            <p>testing</p>--}}
+{{--        </div>--}}
     </div>
 </div>
 {{--    @foreach ($stageplekken as $stageplek)--}}
