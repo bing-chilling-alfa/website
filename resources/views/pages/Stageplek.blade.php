5393a423439e31d56d6b1106588ba74f9b15e845
Fauve <fsetrodinomo@outlook.com>
2022-01-28 11:29:28 +0100

    working photoslider , added pictures

--- resources/views/pages/Stageplek.blade.php
+++ resources/views/pages/Stageplek.blade.php
@@ -13,8 +13,24 @@
             <div class="item active">
                 <img src="la.jpg" alt="Los Angeles" style="width:100%;">
             </div>
+            <div class="item">
+                <img src="chicago.jpg" alt="Chicago" style="width:100%;">
+            </div>
+
+            <div class="item">
+                <img src="people.jpg" alt="Bedrijf" style="width:100%;">
+            </div>
         </div>
+        <a class="left carousel-control" href="#dataCarrousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chrevron-left"></span>
+            <span class="sr-only">Vorige</span>
+        </a>
+        <a class="right carousel-control" href="#dataCarrousel" data-slide="next">
+            <span class="glyphicon glyphicon-chrevron-right"></span>
+            <span class="sr-only">Volgende</span>
+        </a>
     </div>
+</div>
 {{--    <div class="row">--}}
 {{--        <span class="label">Bingchilling</span>--}}
 {{--        <div class="col-sm-06" style="background-color:white" >--}}
