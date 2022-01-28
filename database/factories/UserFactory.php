fefbb3254f76ee19a709b666ef8be9e7a21d8f89
xander <xander.perlee@gmail.com>
2022-01-28 11:34:53 +0100

    Merge branch 'master' of C:\school_werk\applications\xampp\htdocs\school\BingChilling\website with conflicts.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,7 +21,7 @@ class UserFactory extends Factory
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'phone_number' => $this->faker->phoneNumber(),
             'business_id' => $this->faker->unique()->numberBetween(1, 10),
-            'group_id' => $this->faker->numberBetween(1, 1),
+            'group_id' => $this->faker->numberBetween(1, 2),
             'user_type_id' => $this->faker->numberBetween(1, 5),
             'remember_token' => Str::random(10),
         ];
