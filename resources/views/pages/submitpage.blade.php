fd57288c2b04f26c752cbb50207de5ddbdb50690
ronniew <rwaebers@gmail.com>
2022-01-29 18:52:34 +0100

    Update submit page styles

--- resources/views/pages/submitpage.blade.php
+++ resources/views/pages/submitpage.blade.php
@@ -2,29 +2,36 @@
 
 @section('content')
     <div class="container py-3">
-        <div class="row h-10 align-items-sm-center justify-content-center">
-            <div class="col-12 col-sm-9 col-md-7 col-lg-10 col-xl-14">
+        <div class="row h-100 align-items-sm-center justify-content-center">
+            <div class="col-12 col-sm-9 col-md-7 col-lg-5 col-xl-4">
                 <div class="card">
                     <div class="card-body">
-                        <h3 class="card-title">
+                        <h5 class="card-title">
                             Inleverpagina Leerlingen
-                        </h3>
-                        <p>Hier worden de bestanden ingeleverd die gecontrolleerd moeten worden door je begeleider.</p>
-                        <form method="POST" enctype="multipart/form-data">
+                        </h5>
+                        <p class="card-text">Hier worden de bestanden ingeleverd die gecontrolleerd moeten worden door
+                            je begeleider.</p>
+                        <form method="POST" action="{{ route('auth.login') }}" novalidate>
                             @csrf
 
-                            Wat lever je in?
-                            <br />
-                            <input type="text" name="title" />
-                            <br /><br />
-                            Bestand:
-                            <br />
-                            <input type="file" name="Bestand" />
-                            <br /><br />
-                            <input type="submit" value=" Opslaan " />
-                            <input type="reset" value="Verwijderen">
+                            <div>
+                                <label for="name">Wat lever je in?</label>
+                                <input type="text" name="name" class="form-control" id="name"
+                                    placeholder="Een beschrijvende naam van wat je inlevert" />
+                            </div>
+                            <div class="mt-3">
+                                <label for="file">Bestand</label>
+                                <input type="file" name="file" class="form-control" id="file" />
+                            </div>
+                            <div class="mt-3 d-grid">
+                                <button type="submit" class="btn btn-primary">
+                                    Inleveren
+                                </button>
+                                <button type="reset" class="btn btn-secondary mt-2">
+                                    Wissen
+                                </button>
+                            </div>
                         </form>
-
                     </div>
                 </div>
             </div>
