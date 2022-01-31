2823af70e2c49924e056f79864abd2258203265b
NickvanLoo28 <58033512+NickvanLoo@users.noreply.github.com>
2022-01-31 12:30:33 +0100

    Update scss

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,24 +4,7 @@
     {{--    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />--}}
 @endsection
 
-
-
 @section('content')
-    <style>
-        #home {
-            width: 100%;
-            background: url(https://wallpaperaccess.com/full/187161.jpg);
-            height: 50vh;
-        }
-        .home_form   {
-             margin-top: 150px;
-         }
-        .home_form .form {
-            width: calc(80% - 20px );
-            margin: 0 auto;
-        }
-
-    </style>
 <div class="home" id="home">
     <div class="home_form container">
         <div class="justify-content-center" id="mynavbar">
