d1e74bf1121ef305be3e8c08e5f897c54ac97531
Rick Oosting <rp.oosting@student.alfa-college.nl>
2022-02-01 15:05:43 +0100

    Update show.blade.php

--- resources/views/pages/companies/show.blade.php
+++ resources/views/pages/companies/show.blade.php
@@ -1,8 +1,29 @@
 @extends('layouts.default')
 
 @section('content')
+    <div class="container">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title bg-primary text-white">{{ $companies['naam'] }}</h3>
+                    <p class="card-text">
+                    <h5>Wat voor bedrijf zijn wij?</h5>
+                    <p>Onze webwinkel is een uitstekende plaats voor het ontwikkelen van je skills. Je komt in een team met twee zeer ervaren softwareontwikkelaars. Tijdens je stage kom je leuke uiteenlopende opdrachten tegen zoals werken aan de website en backoffice.</p>
+                    <h5>
+                        Informatie bedrijf
+                    </h5>
+                   
+                        {{ $companies['telefoonnummer'] }} <br>
+                        {{ $companies['email'] }} <br>
+                        {{ $companies['website'] }}<br>
+                        {{ $companies['adres'] }}<br>
+                        {{ $companies['postcode'] }}<br>
+                   
+                    </p>
 
-@foreach ($companies as $company)
-{{$company['id'];}}
-@endforeach
+
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
