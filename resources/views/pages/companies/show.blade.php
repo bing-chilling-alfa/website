de5e81e0682fc68509fc420600ddd1b60b3c35a2
Rick Oosting <rp.oosting@student.alfa-college.nl>
2022-02-01 15:23:10 +0100

    Update show.blade.php

--- resources/views/pages/companies/show.blade.php
+++ resources/views/pages/companies/show.blade.php
@@ -5,7 +5,7 @@
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    <h3 class="card-title bg-primary text-white">{{ $companies['naam'] }}</h3>
+                    <h3 class="card-title bg-primary text-white">{{ $companies['name'] }}</h3>
                     <p class="card-text">
                     <h5>Wat voor bedrijf zijn wij?</h5>
                     <p>Onze webwinkel is een uitstekende plaats voor het ontwikkelen van je skills. Je komt in een team met twee zeer ervaren softwareontwikkelaars. Tijdens je stage kom je leuke uiteenlopende opdrachten tegen zoals werken aan de website en backoffice.</p>
@@ -13,11 +13,12 @@
                         Informatie bedrijf
                     </h5>
                    
-                        {{ $companies['telefoonnummer'] }} <br>
+                        {{ $companies['phone_number'] }} <br>
                         {{ $companies['email'] }} <br>
                         {{ $companies['website'] }}<br>
-                        {{ $companies['adres'] }}<br>
-                        {{ $companies['postcode'] }}<br>
+                        {{ $companies['street_name'] }} {{ $companies['house_number'] }}<br>
+                        {{ $companies['zip_code'] }}<br>
+                        {{ $companies['place_name'] }}<br>
                    
                     </p>
 
