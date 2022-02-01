4de6e91431e1d787935e59e9dfa794f0ad299c16
NickvanLoo28 <58033512+NickvanLoo@users.noreply.github.com>
2022-02-01 11:17:55 +0100

    Update student.blade

--- resources/views/pages/dashboard/student.blade.php
+++ resources/views/pages/dashboard/student.blade.php
@@ -35,7 +35,10 @@
         <div class="card">
          <div class="card-title text-white bg-primary justify-content-center"><h4>Stagebedrijf</h4></div>
             <div class="card-body">
-            
+            <h6><b>Bedrijfsnaam</b></h6>
+            <p>Werken met merken B.V.</p>
+            <h6><b>Adres</b></h6>
+            <p>Waardeel 2, 8431 ND Oosterwolde</p>
             </div> 
             </div>      
         </div>
@@ -43,16 +46,20 @@
         <div class="card">
         <div class="card-title text-white bg-primary justify-content-center"><h4>Inleveren</h4></div>
             <div class="card-body">
-            
-             
+            <p>PVA inleveren op 21-02-2022</p>
         </div>
         </div>
         </div>
         <div class="col-md-6">
         <div class="card">
-         <div class="card-title text-white bg-primary justify-content-center"><h4>Stagebedrijf</h4></div>
+         <div class="card-title text-white bg-primary justify-content-center"><h4>School</h4></div>
             <div class="card-body">
-            
+            <h6><b>Naam</b></h6>
+            <p>Alfa-college</p>
+            <h6><b>Adres</b></h6>
+            <p>Boumaboulevard 573, 9723 ZS Groningen</p>
+             <h6><b>Opleiding</b></h6>
+            <p>Applicatie- en mediaontwikkelaar</p>
             </div> 
             </div>      
         </div>
