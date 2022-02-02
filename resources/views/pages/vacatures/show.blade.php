50d73b3b2ac0363e8761a70c36c5dc06fc3158d8
Fauve <festrodinomo@outlook.com>
2022-02-02 12:49:54 +0100

    working on the card software developer

--- resources/views/pages/vacatures/show.blade.php
+++ resources/views/pages/vacatures/show.blade.php
@@ -1,47 +1,17 @@
 @extends('layouts.default')
 
 @section('content')
-
-<div class="container">
-{{--<div class="stageplek align-items-sm-center">--}}
-    <div id="stageCarrousel" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#dataCarousel" data-slide-to="0" class="active"></li>
-            <li data-target="#dataCarousel" data-slide-to="1" ></li>
-            <li data-target="#dataCarousel" data-slide-to="2" ></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="item active">
-                <img src="la.jpg" alt="Los Angeles" style="width:100%;">
-            </div>
-
-            <div class="item">
-                <img src="chicago.jpg" alt="Chicago" style="width:100%;">
-            </div>
-
-            <div class="item">
-                <img src="people.jpg" alt="Bedrijf" style="width:100%;">
-            </div>
-        </div>
-
-        <a class="left carousel-control" href="#dataCarousel" data-slide="prev">
-            <span class="glyphicon glyphicon-chrevron-left"></span>
-            <span class="sr-only">Vorige</span>
-        </a>
-        <a class="right carousel-control" href="#dataCarousel" data-slide="next">
-            <span class="glyphicon glyphicon-chrevron-right"></span>
-            <span class="sr-only">Volgende</span>
-        </a>
+<div class ="container py-3">
+<div class = "col-12">
+<div class="card">
+  <div class="card-body">
+    <h5 class="card-title">Software developer</h5>
+    <div class="card-text">
+    Vind jij het leuk om goede en vooral effectieve websites te bouwen? Ben jij communicatief vaardig en kan je pixels aflezen als een kampioen? Vind je structuur belangrijk en zie je deadlines als een uitdaging? Dan zijn wij op zoek naar jou!
     </div>
+  </div>
 </div>
-{{--    <div class="row">--}}
-{{--        <span class="label">Bingchilling</span>--}}
-{{--        <div class="col-sm-06" style="background-color:white" >--}}
-{{--            <p>testing</p>--}}
-{{--        </div>--}}
-
-{{--    @foreach ($stageplekken as $stageplek)--}}
-
-{{--        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />--}}
+</div>
+</div>
 @endsection
 
