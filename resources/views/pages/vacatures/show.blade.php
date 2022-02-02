09e83c64b7553e98dc6c26afa9b734d959edd484
Fauve <festrodinomo@outlook.com>
2022-02-02 19:51:45 +0100

    working on grid, slider must be on the right

--- resources/views/pages/vacatures/show.blade.php
+++ resources/views/pages/vacatures/show.blade.php
@@ -22,44 +22,46 @@ integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCm
             </div>
         </div>
 
-        <div class="col-6">
-            <div class="card">
-                <div class="card-body">
-                    <h2 class="card-title text-white bg-primary">Wat ga je leren?</h2>
-                    <h5>Realiseert software</h5>
-                    <div class="card-text">
-                        <p>Groningen • MBO 4</p>
-                        <br>
-                        B1-K1-W1 Plant werkzaamheden en bewaakt de voortgang<br>
-                        <br>
-                        B1-K1-W2 Ontwerpt software
-                        <br>
-                        B1-K1-W3 Realiseert (onderdelen van) software
-                        <br>
-                        B1-K1-W4 Test software
-                        <br>
-                        B1-K1-W4 Test software
+        <div class="row">
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h2 class="card-title text-white bg-primary">Wat ga je leren?</h2>
+                        <h5>Realiseert software</h5>
+                        <div class="card-text">
+                            <p>Groningen • MBO 4</p>
+                            <br>
+                            B1-K1-W1 Plant werkzaamheden en bewaakt de voortgang<br>
+                            <br>
+                            B1-K1-W2 Ontwerpt software
+                            <br>
+                            B1-K1-W3 Realiseert (onderdelen van) software
+                            <br>
+                            B1-K1-W4 Test software
+                            <br>
+                            B1-K1-W4 Test software
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="col-6">
-            <div class="card">
-                <div class="card-body">
-                    <h5>Werkt in een ontwikkelteam</h5>
-                    <div class="card-text">
-                        <p>Groningen • MBO 4</p>
-                        B1-K2-W2 Presenteert het opgeleverde werk<br>
-                        <br>
-                        B1-K2-W3 Reflecteert op het werk<br>
+            <div class="col-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h5>Werkt in een ontwikkelteam</h5>
+                        <div class="card-text">
+                            <p>Groningen • MBO 4</p>
+                            B1-K2-W2 Presenteert het opgeleverde werk<br>
+                            <br>
+                            B1-K2-W3 Reflecteert op het werk<br>
 
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
 
-        {{-- <div class="h-75 d-inline-block" style=120px; bg-primary> --}}
+            {{-- <div class="h-75 d-inline-block" style=120px; bg-primary> --}}
+            <div class="col-6 col-md-4"></div>
             <div class="container py-3">
                 <div class="col-6">
                     <div id="CarrouselControls" class="carousel slide" data-ride="carousel">
@@ -100,5 +102,6 @@ integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCm
                 </div>
             </div>
         </div>
+    </div>
 
-    @endsection
+@endsection
