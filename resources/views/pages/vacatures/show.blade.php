344f25a988440647b51f5bd50a56209fe923cb26
Fauve <festrodinomo@outlook.com>
2022-02-02 20:13:19 +0100

    added pictures

--- resources/views/pages/vacatures/show.blade.php
+++ resources/views/pages/vacatures/show.blade.php
@@ -47,98 +47,62 @@ integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCm
 
 
             <div class="col-6">
-             <div id="CarrouselControls" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <li data-target="#CarouselControls" data-slide-to="0" class="active"></li>
-                            <li data-target="#CarouselControls" data-slide-to="1"></li>
-                            <li data-target="#CarouselControls" data-slide-to="2"></li>
-                        </ol>
+                <div class="card">
+                    <div class="card-body">
+                        <div id="CarrouselControls" class="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#CarouselControls" data-slide-to="0" class="active"></li>
+                                <li data-target="#CarouselControls" data-slide-to="1"></li>
+                                <li data-target="#CarouselControls" data-slide-to="2"></li>
+                            </ol>
 
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img class="d-block w-100"
-                                    src="https://st3.depositphotos.com/5392356/13702/i/950/depositphotos_137028018-stock-photo-programmers-cooperating-at-company.jpg?forcejpeg=true"
-                                    alt="First slide">
+                            <div class="carousel-inner">
+                                <div class="carousel-item active">
+                                    <img class="d-block w-100"
+                                        src="https://images.pexels.com/photos/1367269/pexels-photo-1367269.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                                        alt="First slide">
+                                </div>
+
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="https://images.pexels.com/photos/937481/pexels-photo-937481.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                                        alt="Second Slide">
+                                </div>
+                                <div class="carousel-item">
+                                    <img class="d-block w-100"
+                                        src="https://images.pexels.com/photos/443383/pexels-photo-443383.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+                                        alt="Third Slide">
+                                </div>
                             </div>
 
-                            <div class="carousel-item">
-                                <img class="d-block w-100"
-                                    src="https://www.istockphoto.com/nl/foto/social-media-concept-gm1205703732-347429651"
-                                    alt="Second Slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100"
-                                    src="https://www.istockphoto.com/nl/foto/social-media-concept-gm1205703732-347429651"
-                                    alt="Third Slide">
-                            </div>
+                            <a class="carousel-control-prev" href="#CarrouselControls" role="button" data-slide="prev">
+                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Previous</span>
+                            </a>
+                            <a class="carousel-control-next" href="#CarrouselControls" role="button" data-slide="next">
+                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span class="sr-only">Next</span>
+                            </a>
                         </div>
-
-                        <a class="carousel-control-prev" href="#CarrouselControls" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#CarrouselControls" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
                     </div>
                 </div>
-                {{-- <div class="card">
-                    <div class="card-body">
-                        <h5>Werkt in een ontwikkelteam</h5>
-                        <div class="card-text">
-                            <p>Groningen • MBO 4</p>
-                            B1-K2-W2 Presenteert het opgeleverde werk<br>
-                            <br>
-                            B1-K2-W3 Reflecteert op het werk<br>
-
-                        </div>
-                    </div>
-                </div> --}}
             </div>
-
-
-            {{-- <div class="h-75 d-inline-block" style=120px; bg-primary> --}}
-            <div class="container py-3">
+            <div class="row">
                 <div class="col-6">
-                    {{-- <div id="CarrouselControls" class="carousel slide" data-ride="carousel">
-                        <ol class="carousel-indicators">
-                            <li data-target="#CarouselControls" data-slide-to="0" class="active"></li>
-                            <li data-target="#CarouselControls" data-slide-to="1"></li>
-                            <li data-target="#CarouselControls" data-slide-to="2"></li>
-                        </ol>
+                    <div class="card">
+                        <div class="card-body">
+                            <h5>Werkt in een ontwikkelteam</h5>
+                            <div class="card-text">
+                                <p>Groningen • MBO 4</p>
+                                B1-K2-W2 Presenteert het opgeleverde werk<br>
+                                <br>
+                                B1-K2-W3 Reflecteert op het werk<br>
 
-                        <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img class="d-block w-100"
-                                    src="https://st3.depositphotos.com/5392356/13702/i/950/depositphotos_137028018-stock-photo-programmers-cooperating-at-company.jpg?forcejpeg=true"
-                                    alt="First slide">
-                            </div>
-
-                            <div class="carousel-item">
-                                <img class="d-block w-100"
-                                    src="https://www.istockphoto.com/nl/foto/social-media-concept-gm1205703732-347429651"
-                                    alt="Second Slide">
-                            </div>
-                            <div class="carousel-item">
-                                <img class="d-block w-100"
-                                    src="https://www.istockphoto.com/nl/foto/social-media-concept-gm1205703732-347429651"
-                                    alt="Third Slide">
                             </div>
                         </div>
-
-                        <a class="carousel-control-prev" href="#CarrouselControls" role="button" data-slide="prev">
-                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Previous</span>
-                        </a>
-                        <a class="carousel-control-next" href="#CarrouselControls" role="button" data-slide="next">
-                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                            <span class="sr-only">Next</span>
-                        </a>
                     </div>
-                </div> --}}
-            </div>
-        </div>
-    </div>
+                </div>
 
-@endsection
+
+
+            @endsection
