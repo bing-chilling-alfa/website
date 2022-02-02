8442cbe1006c30ed5e8c657a37faafff920748be
Rick Oosting <rp.oosting@student.alfa-college.nl>
2022-02-02 11:27:01 +0100

    tos en showblade

--- resources/views/pages/companies/show.blade.php
+++ resources/views/pages/companies/show.blade.php
@@ -4,18 +4,15 @@
     <div class="container">
         <div class="col-12">
             <div class="card">
-                <div class="card-body">
-                    <h3 class="card-title bg-primary text-white justify-content-center d-flex">{{ $companies['name'] }}
-                    </h3>
-                    <p class="card-text">
+                <h3 class="card-title bg-primary text-white justify-content-center d-flex">{{ $companies['name'] }} </h3>
+                <div class="card-body ">
                     <h5>Wat voor bedrijf zijn wij?</h5>
-                    <p>Onze webwinkel is een uitstekende plaats voor het ontwikkelen van je skills. Je komt in een team met
+                    <p class="card-text">Onze webwinkel is een uitstekende plaats voor het ontwikkelen van je skills. Je komt in een team met
                         twee zeer ervaren softwareontwikkelaars. Tijdens je stage kom je leuke uiteenlopende opdrachten
-                        tegen zoals werken aan de website en backoffice.</p>
+                        tegen zoals werken aan de website en backoffice.</p> <br>
                     <h5>
                         Informatie bedrijf
                     </h5>
-
                     {{ $companies['phone_number'] }} <br>
                     {{ $companies['email'] }} <br>
                     {{ $companies['website'] }}<br>
@@ -23,10 +20,6 @@
                     {{ $companies['house_number'] }}<br>
                     {{ $companies['zip_code'] }}<br>
                     {{ $companies['place_name'] }}<br>
-
-                    </p>
-
-
                 </div>
             </div>
         </div>
@@ -35,24 +28,20 @@
             <div class="col">
                 <div class="card">
                     <div class="card-body">
-                        <p>
-                            title : Software developer <br>
-                            type-opleiding : 2 <br>
-                            start-datum : 1-1-2021 <br>
-                            eind-datum : 1-1-2021 <br>
-                        </p>
+                        Title : Software developer <br>
+                        Type-opleiding : 2 <br>
+                        Start-datum : 1-1-2021 <br>
+                        Eind-datum : 1-1-2021 <br>
                     </div>
                 </div>
             </div>
             <div class="col">
                 <div class="card">
                     <div class="card-body">
-                        <p>
-                            title : Software developer <br>
-                            type-opleiding : 2 <br>
-                            start-datum : 1-1-2021 <br>
-                            eind-datum : 1-1-2021 <br>
-                        </p>
+                        Title : Software developer <br>
+                        Type-opleiding : 2 <br>
+                        Start-datum : 1-1-2021 <br>
+                        Eind-datum : 1-1-2021 <br>
                     </div>
                 </div>
             </div>
--- resources/views/pages/legal/terms-of-service.blade.php
+++ resources/views/pages/legal/terms-of-service.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
     <div class="container py-3">
-        <div class="row h-10 align-items-sm-center justify-content-center">
+        <div class="row h-10 align-items-sm-center justify-content-center ">
             <div class="col-12 col-sm-9 col-md-7 col-lg-10 col-xl-14">
                 <div class="card">
                     <div class="card-body">
-                        <h3 class="card-title">
+                        <h3 class="card-title justify-content-center d-flex ">
                             Algemene voorwaarden en privacybeleid
                         </h3>
                         <p>Bing Chilling, gevestigd aan test, is verantwoordelijk voor de verwerking van persoonsgegevens
