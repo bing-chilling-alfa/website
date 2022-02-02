1708d683a197b8ba13aff3fdd1cc1add8e318f03
Fauve <festrodinomo@outlook.com>
2022-02-02 18:22:32 +0100

    fixed the slider button, working on picture and size

--- resources/views/pages/vacatures/show.blade.php
+++ resources/views/pages/vacatures/show.blade.php
@@ -1,7 +1,10 @@
 @extends('layouts.default')
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 @section('content')
     <div class="container py-3">
@@ -70,6 +73,7 @@
                             src="https://st3.depositphotos.com/5392356/13702/i/950/depositphotos_137028018-stock-photo-programmers-cooperating-at-company.jpg?forcejpeg=true"
                             alt="First slide">
                     </div>
+
                     <div class="carousel-item">
                         <img class="d-block w-100"
                             src="https://www.istockphoto.com/nl/foto/social-media-concept-gm1205703732-347429651"
@@ -82,19 +86,18 @@
                     </div>
                 </div>
 
-                <a class="carousel-control-prev" href="CarrouselControls" role="button" data-slide="prev">
+                <a class="carousel-control-prev" href="#CarrouselControls" role="button" data-slide="prev">
                     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Vorige</span>
+                    <span class="sr-only">Previous</span>
                 </a>
-
-                <a class="carousel-control-next" href="CarrouselControls" role="button" data-slide="next">
+                <a class="carousel-control-next" href="#CarrouselControls" role="button" data-slide="next">
                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Volgende</span>
+                    <span class="sr-only">Next</span>
                 </a>
             </div>
         </div>
 
-{{-- <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        {{-- <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
         <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
         <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
