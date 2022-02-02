3691dce53c4d82f3e68671d12158232271a0f918
Fauve <festrodinomo@outlook.com>
2022-02-02 13:14:29 +0100

    working onrow of 'learning goals'

--- resources/views/pages/vacatures/show.blade.php
+++ resources/views/pages/vacatures/show.blade.php
@@ -7,11 +7,47 @@
   <div class="card-body">
     <h5 class="card-title">Software developer</h5>
     <div class="card-text">
+    <p>Groningen • MBO 4</p>
     Vind jij het leuk om goede en vooral effectieve websites te bouwen? Ben jij communicatief vaardig en kan je pixels aflezen als een kampioen? Vind je structuur belangrijk en zie je deadlines als een uitdaging? Dan zijn wij op zoek naar jou!
     </div>
   </div>
 </div>
 </div>
+
+<div class = "col-6">
+<div class="card">
+  <div class="card-body">
+    <h2 class="card-title text-white bg-primary">Wat ga je leren?</h2>
+    <h5>Realiseert software</h5>
+    <div class="card-text">
+    <p>Groningen • MBO 4</p>
+    <br>
+    B1-K1-W1 Plant werkzaamheden en bewaakt de voortgang<br>
+    <br>
+B1-K1-W2 Ontwerpt software
+<br>
+B1-K1-W3 Realiseert (onderdelen van) software
+<br>
+B1-K1-W4 Test software
+<br>
+B1-K1-W4 Test software    </div>
+  </div>
+</div>
+</div>
+<div class = "col-6">
+<div class="card">
+  <div class="card-body">
+    <h5>Werkt in een ontwikkelteam</h5>
+    <div class="card-text">
+    <p>Groningen • MBO 4</p>
+B1-K2-W2 Presenteert het opgeleverde werk<br>
+<br>
+B1-K2-W3 Reflecteert op het werk<br>
+    
+    </div>
+  </div>
+</div>
+</div>
+</div>
 </div>
 @endsection
-
