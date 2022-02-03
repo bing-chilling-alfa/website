229d5c1250220b084ba1663c6d87ca55592b02c7
Fauve <festrodinomo@outlook.com>
2022-02-03 07:52:39 +0100

    change h5 to h4

--- resources/views/pages/vacatures/show.blade.php
+++ resources/views/pages/vacatures/show.blade.php
@@ -11,7 +11,7 @@ integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCm
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Software developer</h5>
+                    <h4 class="card-title">Software developer</h4>
                     <button class="btn btn-primary bg-primary btn-m">BOL</button>
                     <div class="card-text">
                         <p>Groningen • MBO 4</p>
@@ -29,7 +29,7 @@ integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCm
             <div class="col-6">
                 <div class="card">
                     <div class="card-body">
-                        <h2 class="card-title text-white bg-primary">Wat ga je leren?</h2>
+                        <h5 class="card-title text-white bg-primary">Wat ga je leren?</h5>
                         <h5>Realiseert software</h5>
                         <div class="card-text">
                             <p>Groningen • MBO 4</p>
@@ -44,7 +44,7 @@ integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCm
                         <div class="card-text">
                             <br>
 
-B1-K2-W2 Presenteert het opgeleverde werk
+B1-K2-W2 Presenteert het opgeleverde werk<br>
 <br>
 B1-K2-W3 Reflecteert op het werk
 <br>
